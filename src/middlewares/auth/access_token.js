b7a3153e63128bae163babaa7346ef421f16cef1
Masaya Kawabata <m-kawabata@glad-cube.com>
2020-05-05 18:53:39 +0900

    access_token endpoint 追加

--- src/middlewares/auth/access_token.js
+++ src/middlewares/auth/access_token.js
@@ -19,36 +19,12 @@ generate_access_token_options = (token) => {
 }
 
 module.exports = async (req, res) => {
-  console.log('hgoehogehogehogehoo')
   const options = generate_access_token_options(req.query.request_token)
   try{
-    console.log('---options---')
-    console.log(options)
-    console.log('---options---')
     const result = await axios(options)
-    console.log('---result---')
-    console.log(result)
-    console.log('---result---')
-    return result.data
+    return res.send(result.data)
   }catch(e){
-    const err = generate_err_obj(e)
-    console.log(err)
-    return err
+    const err = error.generate_err_obj(e)
+    return res.send(err)
   }
-  // const options = generate_access_token_options(req.query.request_token)
-  // console.log('-----ここまで来てるよー')
-  // console.log(options)
-  // console.log('-----ここまで来てるよー')
-  // try{
-  //   console.log('>>>try')
-  //   let result = await axios.post(options)
-  //   return res.send(result)
-  // }catch(e){
-  //   console.log('>>>catch')
-  //   const err = error.generate_err_obj(e)
-  //   console.log('-------------err--------------')
-  //   console.log(err)
-  //   console.log('-------------err--------------')
-  //   return res.send(err)
-  // }
 }
